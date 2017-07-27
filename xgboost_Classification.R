c0e4d3d774da59bf3e1ed10c5ddad97270ccf589
Pedro Pablo Franz Duran <pedro.franz@gmail.com>
2017-07-27 12:18:59 -0400

    Update xgboost_Classification.R

--- xgboost_Classification.R
+++ xgboost_Classification.R
@@ -1,11 +1,11 @@
-# Date: Jan 2016
+# Date: Jul 2017
 # xgboost_Classification.R
 # This is an example of xgboost model using the iris data available in base R.
 # To run this code, you need to have the xgboost package installed. You do not have to read in any data files..
 
 # Predict the Species from the 4 features of iris data.
 # The data contains numeric predictors. Our target column is Species, with 3 classes.
- 
+
 # Note: This uses a two step process.
 # Step 1 performs cross-validation to find the number of iterations needed to get the minimum loss.
 # Step 2 creates the final model using the nround identified in Step 1, and makes the prediction. 
@@ -20,7 +20,7 @@ library(caret)      # for confusionMatrix
 #Check the data structure
 data(iris)
 print(str(iris))
- 
+
 #Split the iris data into training (70%) and testing(30%).
 set.seed(100)
 ind = sample(nrow(iris),nrow(iris)* 0.7)
@@ -33,16 +33,16 @@ testing = iris[-ind,]
 #      You can try different values for nthread, max_depth, eta, gamma, etc., and see if you get lower prediction error.
 
 param       = list("objective" = "multi:softmax", # multi class classification
-	      "num_class"= 3 ,  		# Number of classes in the dependent variable.
-              "eval_metric" = "mlogloss",  	 # evaluation metric 
-              "nthread" = 8,   			 # number of threads to be used 
-              "max_depth" = 16,    		 # maximum depth of tree 
-              "eta" = 0.3,    			 # step size shrinkage 
-              "gamma" = 0,    			 # minimum loss reduction 
-              "subsample" = 0.7,    		 # part of data instances to grow tree 
-              "colsample_bytree" = 1, 		 # subsample ratio of columns when constructing each tree 
-              "min_child_weight" = 12  		 # minimum sum of instance weight needed in a child 
-              )
+                   "num_class"= 3 ,  		# Number of classes in the dependent variable.
+                   "eval_metric" = "mlogloss",  	 # evaluation metric 
+                   "nthread" = 8,   			 # number of threads to be used 
+                   "max_depth" = 16,    		 # maximum depth of tree 
+                   "eta" = 0.3,    			 # step size shrinkage 
+                   "gamma" = 0,    			 # minimum loss reduction 
+                   "subsample" = 0.7,    		 # part of data instances to grow tree 
+                   "colsample_bytree" = 1, 		 # subsample ratio of columns when constructing each tree 
+                   "min_child_weight" = 12  		 # minimum sum of instance weight needed in a child 
+)
 
 #Identify the Predictors and the dependent variable, aka label.
 predictors = colnames(training[-ncol(training)])
@@ -53,7 +53,7 @@ print(table (label))
 #Alas, xgboost works only if the numeric labels start from 0. Hence, subtract 1 from the label.
 label = as.numeric(training[,ncol(training)])-1
 print(table (label))
-		  
+
 #########################################################################################################
 # Step 1: Run a Cross-Validation to identify the round with the minimum loss or error.
 #         Note: xgboost expects the data in the form of a numeric matrix.
@@ -62,19 +62,19 @@ set.seed(100)
 
 cv.nround = 200;  # Number of rounds. This can be set to a lower or higher value, if you wish, example: 150 or 250 or 300  
 bst.cv = xgb.cv(
-        param=param,
-	data = as.matrix(training[,predictors]),
-	label = label,
-	nfold = 3,
-	nrounds=cv.nround,
-	prediction=T)
+  param=param,
+  data = as.matrix(training[,predictors]),
+  label = label,
+  nfold = 3,
+  nrounds=cv.nround,
+  prediction=T)
 
 #Find where the minimum logloss occurred
-min.loss.idx = which.min(bst.cv$dt[, test.mlogloss.mean]) 
+min.loss.idx = which.min(bst.cv$evaluation_log[, test_mlogloss_mean]) 
 cat ("Minimum logloss occurred in round : ", min.loss.idx, "\n")
 
 # Minimum logloss
-print(bst.cv$dt[min.loss.idx,])
+print(bst.cv$evaluation_log[min.loss.idx,])
 
 ##############################################################################################################################
 # Step 2: Train the xgboost model using min.loss.idx found above.
@@ -82,10 +82,10 @@ print(bst.cv$dt[min.loss.idx,])
 set.seed(100)
 
 bst = xgboost(
-		param=param,
-		data =as.matrix(training[,predictors]),
-		label = label,
-		nrounds=min.loss.idx)
+  param=param,
+  data =as.matrix(training[,predictors]),
+  label = label,
+  nrounds=min.loss.idx)
 
 # Make prediction on the testing data.
 testing$prediction = predict(bst, as.matrix(testing[,predictors]))
@@ -109,4 +109,4 @@ testing$prediction.rf = predict(model.rf,testing[,predictors])
 #Compute the accuracy of predictions.
 confusionMatrix( testing$prediction.rf,testing$Species)
 ################################################################################################################################
-####################################
+###################################
